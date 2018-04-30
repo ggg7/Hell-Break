442f4ccc69cac601574bb6c5707e7e7126a97530
ggg7 <ggg7@njit.edu>
2018-04-30 19:39:11 -0400

    WORK5

---
+++ Assets/DoorOpen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+    public GameObject Door;
+    public bool isPressed;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        if (isPressed == true)
+        {
+            anim.SetTrigger("DoorOpen");
+            anim.SetTrigger("SwitchFlipped");
+        }
+	}
+	
+	// Update is called once per frame
+	void KeyPressed()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            isPressed = true;
+        }
+    }
+}
